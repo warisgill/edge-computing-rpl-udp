9805c2177b6c24e8b708ea6eb92ab04aa1d86684
Waris Gill <wgill18@ku.edu.tr>
2019-11-19 13:57:33 +0300

    server file update

--- python-scripts/server.py
+++ python-scripts/server.py
@@ -3,16 +3,17 @@ import socket
 import time
 
 class Server:
-    def __init__(self, ip = None, port = None):
+    def __init__(self, ip = None, port = None, tcp= False):
         super().__init__()
         self._ip = ip
         self._port = port
+        self._tcp = tcp
 
         if ip is None:
             raise Exception("Please enter correct ip and port of the server.")
     
-    # initiate a TCP connection
-    def listen(self):
+
+    def tcp(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             count = 0
             t1 = time.time()
@@ -35,15 +36,37 @@ class Server:
                     if not data:
                         break
                 print(">Client Disconnected ", addr)
+    
+    def udp(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM ) as s:
+            count = 0
+            t1 = time.time()
+            s.bind((self._ip,self._port))
+            print(">Gateway/Cloud: IP ", s)
+            
+            while True:
+                data, addr = s.recvfrom(1024)
+                count += 1
+                if count == 1:
+                    t1 = time.time() 
+                print(">Cloud/Cloudlet: # of Pakcets Received: {p}, Time in Seconds: {t}".format(p= count, t = '{0:.2f}'.format(time.time()-t1)))
+                
+            print(">Client Disconnected ", addr)
+
+
+    # initiate a TCP connection
+    def listen(self):
+       if self._tcp:
+           self.tcp()
+       else:
+           self.udp()  
+
 
 
 if __name__ == "__main__":
+
     port = 8080
     ip = ""
 
-    s = Server(ip = ip, port = port)
+    s = Server(ip = ip, port = port, tcp=False)
     s.listen()
-
-    
-
-
